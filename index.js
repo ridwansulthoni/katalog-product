f6e5d9a95cab717fa0b2c8a4f2f29499d2c0f532
ridwansulthoni <ridwansulthonie@gmail.com>
2024-07-25 12:09:59 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello World Kak');
 })
 
+app.post('/product', (req, res) => {
+    console.log({ requestFromOutside: req.body});
+    res.send('Data berhasil dibuat');
+})
 app.listen(port, ()=> {
     console.log(`Running on port ${port}`);
 })

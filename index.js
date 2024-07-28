39b83ff6606a03ae32e73c58ea7cb34679d945f4
ridwansulthoni <ridwansulthonie@gmail.com>
2024-07-28 10:00:48 +0700

    update

--- index.js
+++ index.js
@@ -4,19 +4,64 @@ const port = 3000;
 const db = require('./connection');
 const response = require('./response');
 
-app.get('/', (req, res) => {
+app.get('/api/v1/produk', (req, res) => {
+    db.query("SELECT * FROM produk", (error, result)=> {
+        response(200, result, "get all data product", res);
+    })
+})
+app.post('/api/v1/produk', (req, res) => {
+    db.query("SELECT * FROM produk", (error, result)=> {
+        response(200, result, "get all data product", res);
+    })
+})
+app.put('/api/v1/produk', (req, res) => {
+    db.query("SELECT * FROM produk", (error, result)=> {
+        response(200, result, "get all data product", res);
+    })
+})
+app.delete('/api/v1/produk', (req, res) => {
     db.query("SELECT * FROM produk", (error, result)=> {
         response(200, result, "get all data product", res);
     })
 })
 
-app.get('/kategori', (req, res)=> {
+app.get('/api/v1/kategori', (req, res)=> {
+    db.query("SELECT * FROM kategori", (error,result) =>{
+        response(200, result, "get all data kategori", res);
+    })
+})
+app.post('/api/v1/kategori', (req, res)=> {
+    db.query("SELECT * FROM kategori", (error,result) =>{
+        response(200, result, "get all data kategori", res);
+    })
+})
+app.put('/api/v1/kategori', (req, res)=> {
+    db.query("SELECT * FROM kategori", (error,result) =>{
+        response(200, result, "get all data kategori", res);
+    })
+})
+app.delete('/api/v1/kategori', (req, res)=> {
     db.query("SELECT * FROM kategori", (error,result) =>{
         response(200, result, "get all data kategori", res);
     })
 })
 
-app.get('/merek', (req, res)=> {
+app.get('/api/v1/merek', (req, res)=> {
+    db.query("SELECT * FROM merek", (error, result)=> {
+        response(200, result, "get all data merek", res);
+    })
+})
+app.post('/api/v1/merek', (req, res)=> {
+    db.query("SELECT * FROM merek", (error, result)=> {
+        response(200, result, "get all data merek", res);
+    })
+})
+app.put('/api/v1/merek', (req, res)=> {
+    db.query("SELECT * FROM merek", (error, result)=> {
+        response(200, result, "get all data merek", res);
+    })
+})
+app.delete('/api/v1/merek', (req, res)=> {
     db.query("SELECT * FROM merek", (error, result)=> {
         response(200, result, "get all data merek", res);
     })

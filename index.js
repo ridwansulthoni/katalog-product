36378c6de7b8048e370d9d4d68eacfba27a5d865
ridwansulthoni <ridwansulthonie@gmail.com>
2024-07-29 15:38:47 +0700

    update

--- index.js
+++ index.js
@@ -1,27 +1,68 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
 const db = require('./connection');
 const response = require('./response');
 
+app.use(bodyParser.json());
+
 app.get('/api/v1/produk', (req, res) => {
-    db.query("SELECT * FROM produk", (error, result)=> {
+    const sql = `SELECT * FROM produk`
+    db.query(sql, (err, result)=> {
+        if (err) throw err;
         response(200, result, "get all data product", res);
     })
 })
+
 app.post('/api/v1/produk', (req, res) => {
-    db.query("SELECT * FROM produk", (error, result)=> {
-        response(200, result, "get all data product", res);
+    const { namaProduk,deskripsi,harga,stok,kategori,merek,foto } = req.body
+    const sql = `INSERT INTO produk (Nama_Produk, Deskripsi, Harga, Stok, ID_Kategori, ID_Merek, Gambar) VALUES ('${namaProduk}','${deskripsi}','${harga}','${stok}','${kategori}','${merek}','${foto}')`
+    db.query(sql, (err, result)=> {
+        if (err) response(500, "Invalid", "Error", res);
+        if (result?.affectedRows) {
+            const data = {
+                isSuccess: result.affectedRows,
+                id: result.insertId,
+            }
+            response(200, data, "add data success", res);
+        }
     })
 })
+
 app.put('/api/v1/produk', (req, res) => {
-    db.query("SELECT * FROM produk", (error, result)=> {
-        response(200, result, "get all data product", res);
+    const { idProduk, namaProduk,deskripsi,harga,stok,kategori,merek,foto } = req.body
+    const sql = `UPDATE produk SET Nama_Produk = '${namaProduk}', Deskripsi = '${deskripsi}', 
+                Harga = '${harga}', Stok = '${stok}', ID_Kategori = '${kategori}', 
+                ID_Merek = '${merek}', Gambar = '${foto}' WHERE ID_Produk = '${idProduk}'`
+
+    db.query(sql, (err, result)=> {
+        if (err) response(500, "Invalid", "Error", res);
+        if (result?.affectedRows) {
+            const data = {
+                isSuccess: result.affectedRows,
+                message: result.message,
+            }
+            response(200, data, "update data success", res);
+        } else {
+            response(404, "produk not found", "error", res);
+        }
     })
 })
+
 app.delete('/api/v1/produk', (req, res) => {
-    db.query("SELECT * FROM produk", (error, result)=> {
-        response(200, result, "get all data product", res);
+    const { idProduk} = req.body
+    const sql = `DELETE FROM produk WHERE ID_Produk = '${idProduk}'`
+    db.query(sql, (err, result)=> {
+        if (err) response(500, "Invalid", "Error", res);
+        if (result?.affectedRows) {
+            const data = {
+                idDeleted: result.affectedRows
+            }
+            response(200, data, "delete data success", res);
+        } else {
+            response(404, "produk not found", "error", res);
+        }
     })
 })
 
